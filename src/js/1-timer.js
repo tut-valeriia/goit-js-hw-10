b4e8a26161733000f44c6f929da0c19a0a0a67c3
tut-valeriia <tut.valeriya@gmail.com>
2026-08-04 21:05:24 +0300

    feat: complete homework 10

---
+++ src/js/1-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const datetimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(datetimePicker, options);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimerInterface({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  datetimePicker.disabled = true;
+
+  timerId = setInterval(() => {
+    const currentTime = new Date();
+    const ms = userSelectedDate - currentTime;
+
+    if (ms <= 0) {
+      clearInterval(timerId);
+      datetimePicker.disabled = false;
+      updateTimerInterface({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      return;
+    }
+
+    const timeComponents = convertMs(ms);
+    updateTimerInterface(timeComponents);
+  }, 1000);
+});
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        icon: false,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        icon: false,
+      });
+    });
+});
